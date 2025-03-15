f26f5f8c51a2509b3a175864b2bff91a4a48db17
janek_szyndlarewicz <jan.szyndlarewicz@gmail.com>
2025-03-15 15:27:49 +0100

    logger update

--- event4_consumer/event4_consumer/config/logger.py
+++ event4_consumer/event4_consumer/config/logger.py
@@ -1,7 +1,15 @@
 import logging
+import os
+
+if not os.path.exists('logs'):
+    os.makedirs('logs')
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("logs/app.log"),
+        logging.StreamHandler()
+    ]
 )
 logger = logging.getLogger(__name__)
